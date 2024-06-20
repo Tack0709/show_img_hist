73267b1d00df593dc5ebbd28a4fc08a469379efc
Takuma Mizuno <34719035ict.nitech.ac.jp>
2024-06-20 11:04:36 +0900

    change caption text

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons',
+        caption='image from wikimedia commons,2024/06/06',
         use_column_width=True
     )
 
